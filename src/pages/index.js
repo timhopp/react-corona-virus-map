1e3d6a23f6bfb1cd439324e73fcdd1d94892e5a6
tim hopp <timothyjhopp@gmail.com>
2020-10-12 15:36:35 -0600

    added data to map and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ import Map from "components/Map";
 import axios from "axios";
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -44,6 +44,73 @@ const IndexPage = () => {
     }
 
     const { data = [] } = response;
+
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    const geoJson = {
+      type: "FeatureCollection",
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: "Feature",
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`;
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
+
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: "icon",
+            html,
+          }),
+          riseOnHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo(map);
   }
 
   // const { data = [] } = response;
